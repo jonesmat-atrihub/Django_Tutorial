d80ab1b0547db1dc04661dd78bd6589cf3f19a23
Matthew Jones <jonesmat@atrihub.io>
2020-09-22 14:16:43 -0700

    feat (MJ Django): complete Part 5
    Completed Part 5 of the Django tutorial
    
    [ICTRAINING-462]

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,21 +3,29 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+
+class Question( models.Model ):
+    question_text = models.CharField( max_length=200 )
+    pub_date = models.DateTimeField( 'date published' )
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - \
-               datetime.timedelta(days=1)
+        """
+        Checks that pub_date is within one day ago
+        AND not in the future
+        :return: whether the pub_date was within the last day
+        """
+        now = timezone.now()
+        return now - datetime.timedelta( days=1 ) <= \
+               self.pub_date <= now
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+
+class Choice( models.Model ):
+    question = models.ForeignKey( Question, on_delete=models.CASCADE )
+    choice_text = models.CharField( max_length=200 )
+    votes = models.IntegerField( default=0 )
 
     def __str__(self):
         return self.choice_text
